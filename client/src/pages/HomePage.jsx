13397e8f9c1b356de7b8ae100f18667887193fa9
Musire <Mcmusire@gmail.com>
2023-05-15 14:11:09 -0600

    finishing touches

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,14 +1,11 @@
-import { SlideProvider } from "../context/SlideContext";
 import { Hero, About, Slider } from "./"
 
 const HomePage = () => {
     return ( 
         <section className="bg-white xs:w-full lg:w-4/5 desktop:w-5/6 h-screen md:grid md:grid-cols-[1.5fr_1.1fr] md:grid-rows-[60%_50%]">
-            <SlideProvider >
-                <Slider />
-            </SlideProvider>
+            <Slider />
             <Hero />
-            <About />          
+            <About />      
         </section>
      );
 }
